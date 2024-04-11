60c84dbbe0addeb6f8df54d349f20ec181084979
Carlos Gabriel S. Pereira <carlosgabriel.sp@live.com>
2024-04-11 15:34:37 -0300

    Corrige bug da hora de criação adiantada

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ use Cosanpa\Src\Router;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
+date_default_timezone_set('America/Belem');
 session_start();
 
 $rotas = require_once __DIR__ . "/../config/rotas.php";
